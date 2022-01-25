61c9b0667c48f185b3aee9a55a4adbca60cb5358
Joanna Christian <joanna.s.christian@gmail.com>
2022-01-24 20:59:42 -0800

    Completed: Location Search Modal to display on Mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,24 @@
-$(document).ready(function(){
-  $(".modal").addClass("is-active");
-  $("#lanuchModal").click(function() {
-    $(".modal").addClass("is-active");  
-  });
-  
-  $(".modal-close").click(function() {
-     $(".modal").removeClass("is-active");
-  });
-  
-  $("#closebtn").click(function() {
-     $(".modal").removeClass("is-active");
-  });
-  });
-  
-/*
 
-  window.addEventListener("resize", function (){
-      //console.log(window.innerHeight)
-    //  console.log(window.innerWidth)
-    if (window.innerWidth < 768){
-       // $('#mobile-search').modal("show");
-        $('#mobile-search').classList.add('is-active');
+//Display location search modal on mobile (screen size < 768)
+window.addEventListener('resize', function(){
+  if (window.innerWidth < 768){
+    $(document).ready(function(){
+      $(".modal").addClass("is-active");
+      $("#lanuchModal").click(function() {
+        $(".modal").addClass("is-active");  
+      });
+      
+      $(".modal-close").click(function() {
+         $(".modal").removeClass("is-active");
+      });
+      
+      $("#closebtn").click(function() {
+         $(".modal").removeClass("is-active");
+      });
+      });
     }
     else {
-      //  $('#mobile-search').modal("hide");
-        $('#mobile-search').classList.remove('is-active')
-
+            //  $('#mobile-search').modal("hide");
+            $('.modal').classList.remove('is-active')
     }
-  })
-  */
+});
