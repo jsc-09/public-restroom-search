b293a402314c519d269f7dc14b8b4c785d492fec
Joseph McArdle <slater.mcardle@outlook.com>
2022-01-28 22:55:56 -0800

    Got geocoder and map markers working on desktop version. mobile needs work.

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@ var fetchButton = document.querySelector(".fetch-button");
 var locationArray = [];
 
 // fetch request
-function getApi() {
+function getApi(coordinates) {
+  console.log(typeof coordinates);
   var requestUrl =
-    "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=33.194634&lng=-117.379186";
+    `https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=${coordinates[1]}&lng=${coordinates[0]}`;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -35,32 +36,40 @@ function getApi() {
                     <div class="card-content custom-card mt-1">
                       <div class="content">
                           <h4>${filteredResults[i].name}</h4>
-                            <div class="row columns">
-                              <div class="column">
-                                <p>${filteredResults[i].street + ", " + filteredResults[i].city}</p>
-                                <p>${"Distance Away: " + Math.round(distanceAway * 100) / 100 + ' miles'}</p>
-                              </div>
-                              <div class="column">
-                              ${accessible}
-                              ${baby}
-                              ${gender}
-                              </div >
-                            </div >
-                            <div class="row">
-                              <p>${"<strong>Directions: </strong> " + filteredResults[i].directions}</p>
-                              <p>${"<strong>Comments: </strong> " + filteredResults[i].comment}</p>
+                          <div class="row columns">
+                            <div class="column">
+                              <p>${filteredResults[i].street + ", " + filteredResults[i].city}</p>
+                              <p>${"Distance Away: " + Math.round(distanceAway * 100) / 100 + ' miles'}</p>
                             </div>
-                      </div >
-                    </div >
-        `
+                            <div class="column">
+                            ${accessible}
+                            ${baby}
+                            ${gender}
+                            </div >
+                          </div >
+                          <div class="row">
+                            <p>${"<strong>Directions: </strong> " + filteredResults[i].directions}</p>
+                            <p>${"<strong>Comments: </strong> " + filteredResults[i].comment}</p>
+                          </div>
+                      </div>
+                    </div>
+                  `
 
         let locationObject = { 'lat': filteredResults[i].latitude, 'long': filteredResults[i].longitude };
         locationArray.push(locationObject)
 
         restroomInfo.innerHTML += cardmarkup;
 
+        // const el = document.createElement('div');
+        // el.addClass('marker');
+        // const popup = new mapboxgl.Popup({ offset: 25 }).setText(filteredResults[i].name)
+        // new mapboxgl.Marker()
+        //     .setLngLat([filteredResults[i].longitude, filteredResults[i].latitude])
+        //     // .setPopup(popup) // sets a popup on this marker
+        //     .addTo(map);
+        const marker = new mapboxgl.Marker().setLngLat([filteredResults[i].longitude, filteredResults[i].latitude]);
+        marker.addTo(map);
       }
-
     });
 }
 
