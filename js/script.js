4b4a63b2973257f61a02007732b859121b9e8a1b
Joanna Christian <joanna.s.christian@gmail.com>
2022-01-28 20:43:11 -0800

    fixed icon issue

--- js/script.js
+++ js/script.js
@@ -1,35 +1,24 @@
-var fetchButton = document.querySelector(".fetch-button");
-var locationArray = [];
+let fetchButton = document.querySelector(".fetch-button");
+let locationArray = [];
 
 // fetch request
-function getApi() {
-  var requestUrl =
+async function getApi() {
+  let requestUrl =
     "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&lat=33.194634&lng=-117.379186";
 
-  fetch(requestUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      let restroomInfo = document.getElementById('restroomCard');
-      restroomInfo.innerHTML = "";
-      let filteredResults = filterResults(data);
+  let response = await fetch(requestUrl);
+  let data = await response.json();
+  let restroomInfo = document.getElementById('restroomCard');
+  restroomInfo.innerHTML = "";
+  let filteredResults = filterResults(data);
 
-      for (var i = 0; i < filteredResults.length; i++) {
-        var locationName = document.createElement("h3");
-        var city = document.createElement("p");
-        var icons = document.createElement("i");
-        let distanceAway = filteredResults[i].distance;
+  for (let i = 0; i < filteredResults.length; i++) {
+    let distanceAway = filteredResults[i].distance;
+    let accessible = (filteredResults[i].accessible) ? "fab fa-accessible-icon" : "";
+    let baby = (filteredResults[i].changing_table) ? "fas fa-baby" : "";
+    let gender = (filteredResults[i].unisex) ? "fas fa-transgender-alt" : "";
 
-        if (filteredResults[i].accessible === true) {
-          var accessible = "fab fa-accessible-icon";
-        } if (filteredResults[i].changing_table === true) {
-          var baby = " fas fa-baby";
-        } if (filteredResults[i].unisex === true) {
-          var gender = " fas fa-transgender-alt";
-        }
-
-        let cardmarkup = `
+    let cardmarkup = `
                     <div class="card-content custom-card mt-1">
                       <div class="content">
                           <h4>${filteredResults[i].name}</h4>
@@ -39,9 +28,9 @@ function getApi() {
                                 <p>${"Distance Away: " + Math.round(distanceAway * 100) / 100 + ' miles'}</p>
                               </div>
                               <div class="column">
-                                <i id="icons" class="${accessible}"></i>
-                                <i id="icons" class="${baby}"></i>
-                                <i id="icons" class="${gender}"></i>
+                                <i class="${accessible}"></i>
+                                <i class="${baby}"></i>
+                                <i class="${gender}"></i>
                               </div >
                             </div >
                             <div class="row">
@@ -52,16 +41,14 @@ function getApi() {
                     </div >
         `
 
-        let locationObject = { 'lat': filteredResults[i].latitude, 'long': filteredResults[i].longitude };
-        locationArray.push(locationObject)
+    let locationObject = { 'lat': filteredResults[i].latitude, 'long': filteredResults[i].longitude };
+    locationArray.push(locationObject)
 
-        restroomInfo.innerHTML += cardmarkup;
+    restroomInfo.innerHTML += cardmarkup;
 
-      }
-      // console.log(locationArray);
+  }
+  // console.log(locationArray);
 
-    });
-}
-
+};
 // button event listener
 fetchButton.addEventListener("click", getApi);
